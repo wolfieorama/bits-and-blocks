b2d5fb4274899d34ed71de5603f4c62c5ca56668
John Munyi <wachirawamunyi@gmail.com>
2018-01-25 11:46:06 +0200

    added public, signing and verifying key

--- keys.py
+++ keys.py
@@ -1,8 +1,15 @@
 from bitcoin import *
+import ecdsa
+from ecdsa import SigningKey, SECP256k1 # cryptography lib for bitcoin addresses
 import os
 
 def keys_address():
     private_key = os.urandom(32)
     print("private key: ", binascii.hexlify(private_key).decode())
+    signing_key = ecdsa.SigningKey.from_string(private_key, curve = ecdsa.SECP256k1)
+    verifying_key = signing_key.get_verifying_key()
+
+    public_key = bytes.fromhex("04") + verifying_key.to_string()
+    print("public key: ", binascii.hexlify(public_key).decode())
 
 keys_address()
